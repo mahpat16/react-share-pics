24afd2961a6ee969500b9b7fa8b6821527cce495
maheshpatil2016 <mahesh.patil@nutanix.com>
2020-07-12 16:07:00 -0700

    Sharing

--- App.js
+++ App.js
@@ -39,15 +39,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      uri: "https://images.unsplash.com/photo-1594046243098-0fceea9d451e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      photoIndex: 1,
-      imgW: 400,
-      imgH: 600,
       sharedImg: null,
       photos: [],
     }
-    this.photos = ["https://upload.wikimedia.org/wikipedia/commons/5/5d/M%C3%BCnster%2C_Historisches_Rathaus_--_2018_--_1211-29.jpg",
-                   "file:///storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200708-WA0013.jpg"];
     this.pubnub = new PubNub({
       publishKey: 'pub-c-f4a5342c-6ad7-45f0-8239-6c44d94c2305',
       subscribeKey: 'sub-c-c5207010-bf0c-11ea-a57f-4e41fc185ce6',
@@ -57,39 +51,57 @@ class App extends Component {
     });
 
     this.listener = {
-        message: envelope => {
-          if (envelope.message.img != null) {
-            console.log("GGot draw message " + envelope.message.img.length);
-            newImg = 'data:image/jpeg;base64,' + envelope.message.img;
-            this.setState({sharedImg: newImg, imgW:320, imgH: 640});
-          }
+      message: envelope => {
+        if (envelope.message.img != null) {
+          console.log("Got draw message " + envelope.message.img.length);
+          let newImg = 'data:image/jpeg;base64,' + envelope.message.img;
+          this.setState({sharedImg: newImg});
         }
-      };
+      }
+    };
 
     this.pubnub.addListener(this.listener);
     this.pubnub.subscribe({ channels: ["draw"] })
-
-    this.compressImg = async(img) => {
-      let compression = 90;
-      while (compression > 50) {
-        compression -= 3;
-        try {
-          const rtn = await ImageResizer.createResizedImage(img, 320, 640, 'JPEG', compression);
-          const {uri} = rtn;
-          const base64URI = await RNFS.readFile(uri, 'base64');
-          if (base64URI.length < 25000) {
-            return base64URI;
-          }
-        } catch(e) {
-          console.log("compressImg: Error: " + e)
-          return null;
-        }
-      }
-    }
   }
 
-  _handleButtonPress = () => {
-    console.log("In get photos button");
+
+  compressImg = async(img) => {
+    let compression = 90;
+    while (compression > 30) {
+      compression -= 3;
+      try {
+        const rtn = await ImageResizer.createResizedImage(img, 200, 400, 'JPEG', compression);
+        const {uri} = rtn;
+        const base64URI = await RNFS.readFile(uri, 'base64');
+        if (base64URI.length < 25000) {
+          return base64URI;
+        }
+      } catch(e) {
+        console.log("compressImg: Error: " + e)
+        return null;
+      }
+    }
+    console.log("Could not compress below 25K")
+  }
+
+  handleShareImage = async (newuri) => {
+    console.log("handleShareImage: " + newuri);
+    const compressedImg = await this.compressImg(newuri);
+    if (compressedImg == null) {
+      console.log("Failed to compress uri: " + newuri);
+      return;
+    }
+    console.log("Base64: " + compressedImg.length);
+    const message = {
+      img: compressedImg
+    };
+    console.log("Publishing to pubnub")
+    this.pubnub.publish({channel: "draw", message});
+    console.log("Done publishing")
+  }
+
+  handleLoadImages = () => {
+    console.log("handleLoadImages");
     CameraRoll.getPhotos({
         first: 20,
         assetType: 'Photos',
@@ -103,74 +115,68 @@ class App extends Component {
       });
   };
 
-  _handleTouchPress = (uri) => {
-    console.log("Pressed: " + uri);
+  hasAndroidPermission = async() => {
+    console.log("Asking for permissions")
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+  
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (!hasPermission) {
+      await PermissionsAndroid.request(permission);
+    }
+
+    const rPermission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+  
+    const rhasPermission = await PermissionsAndroid.check(rPermission);
+    if (!rhasPermission) {
+      await PermissionsAndroid.request(rPermission);
+    }
+    console.log("Done with permissions")
   }
 
   render() {
-    async function hasAndroidPermission() {
-      console.log("Asking for permissions")
-      const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-    
-      const hasPermission = await PermissionsAndroid.check(permission);
-      if (!hasPermission) {
-        await PermissionsAndroid.request(permission);
-      }
-
-      const rPermission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
-    
-      const rhasPermission = await PermissionsAndroid.check(rPermission);
-      if (!rhasPermission) {
-        await PermissionsAndroid.request(rPermission);
-      }
-      console.log("Done with permissions")
-    }
-
-    const buttonClick = async() => {
-      var newIndex = (this.state.photoIndex + 1) % this.photos.length;
-      console.log("Photos index is: " + newIndex);
-      var newuri = this.photos[newIndex];
-      console.log("Photos uri is: " + newuri);
-      const compressedImg = await this.compressImg(newuri);
-      if (compressedImg == null) {
-        this.setState({photoIndex: newIndex});
-        return;
-      }
-      console.log("Base64 of img is: " + compressedImg.length);
-      const message = {
-        img: compressedImg
-      };
-      console.log("Publishing to pubnub")
-      this.pubnub.publish({channel: "draw", message});
-      console.log("Done publishing")
-    }
-
-    hasAndroidPermission();
-
+    this.hasAndroidPermission();
     return (
-      <PubNubProvider client={this.pubnub}>
-      <View style={styles.imageContainer}>
-        <Button title={"Change Image"} onPress={buttonClick}/>
-        <Image style={{height: this.state.imgH, width: this.state.imgW}} source={{ uri: this.state.sharedImg}} />
-        <Button title="Load Images" onPress={this._handleButtonPress} />
-        <ScrollView horizontal={true} style={{flex: 1}}>
-          {this.state.photos.map((p, i) => {
-          return (
-            <TouchableHighlight
-              key={i}
-              activeOpacity={0.2} 
-              underlayColor='#000000' 
-              onPress={() => this._handleTouchPress(p.node.image.uri)}>
-              <Image
-                style={styles.thumbnail} 
-                source={{ uri: p.node.image.uri }}
-              />
-            </TouchableHighlight>
-          );
-        })}
+      <PubNubProvider style={{flex:1}} client={this.pubnub}>
+        <MyApp 
+          style={{flex:1}} 
+          photos={this.state.photos} 
+          handleLoadImages={this.handleLoadImages} 
+          handleShareImage={this.handleShareImage} 
+          sharedImg={this.state.sharedImg}/>
+      </PubNubProvider>
+    );
+  }
+}
+
+class MyApp extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
+  }
+
+  render() {
+    let {photos, sharedImg, handleShareImage, handleLoadImages} = this.props;
+    return (
+      <View style={{flex:1}}>
+        <Image style={styles.imagefull} source={{ uri: sharedImg}} />
+        <Button style={{flex:1}} title="Load Images" onPress={handleLoadImages} />
+        <ScrollView horizontal={true} style={{flex: 3}}>
+          {photos.map((p, i) => {
+            return (
+              <TouchableHighlight
+                key={i}
+                activeOpacity={0.1} 
+                underlayColor='#FFFFFF' 
+                onPress={() => handleShareImage(p.node.image.uri)}>
+                <Image
+                  style={styles.thumbnail} 
+                  source={{ uri: p.node.image.uri }}
+                />
+              </TouchableHighlight>);
+            })
+          }
         </ScrollView>
       </View>
-      </PubNubProvider>
     )
   }
 }
@@ -219,6 +225,12 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  imagefull: {
+    flex: 8,
+    resizeMode: "cover",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   thumbnail: {
     flex: 1,
